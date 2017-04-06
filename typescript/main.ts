3f34c05fd59608cda447a9799674dbae7e924899
Reed Jones <develop@reedjones.com>
2017-04-05 18:24:59 -0600

    re-wrote writeup

--- typescript/main.ts
+++ typescript/main.ts
@@ -4,6 +4,7 @@ function makeDivs(){
 	var title = document.getElementById("title"),
 		subtitle = document.getElementById("subtitle"),
 		aboutMe = document.getElementById("about-me"),
+		contactMe = document.getElementById("contact-me"),
 
 		title_r = document.createElement("div"),
 		title_t = document.createElement("div"),
@@ -12,7 +13,10 @@ function makeDivs(){
 		subtitle_l = document.createElement("div"),
 
 		aboutMe_t = document.createElement("div"),
-		aboutMe_l = document.createElement("div");
+		aboutMe_l = document.createElement("div"),
+
+
+		contactMe_t = document.createElement("div");
 
 	title_r.classList.add("titleBorderRight");
 	title_t.classList.add("titleBorderTop");
@@ -20,6 +24,7 @@ function makeDivs(){
 	subtitle_l.classList.add("subtitleBorderLeft");
 	aboutMe_t.classList.add("aboutMeBorderTop");
 	aboutMe_l.classList.add("aboutMeBorderLeft");
+	contactMe_t.classList.add("contactMeBorderTop");
 
 	title.appendChild(title_r);
 	title.appendChild(title_t);
@@ -27,6 +32,7 @@ function makeDivs(){
 	subtitle.appendChild(subtitle_l);
 	aboutMe.appendChild(aboutMe_t);
 	aboutMe.appendChild(aboutMe_l);
+	contactMe.appendChild(contactMe_t);
 
 	setTimeout(() => {
 		title_r.style.bottom =  "-100%";
@@ -37,5 +43,9 @@ function makeDivs(){
 
  		aboutMe_t.style.left = "66%";
  		aboutMe_l.style.bottom = "0";
+
+ 		 contactMe_t.style.left = "0";
+ 		// contactMe_t.style.right = "0";
+ 		contactMe_t.style.width = "100%";
 	}, 10);
 }
