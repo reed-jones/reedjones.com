78cf3eb42eec558573e66a39c37a9c812d51825d
Reed Jones <reed.jones@pderas.com>
2020-05-19 22:14:04 -0600

    update projects

--- assets/sw.js
+++ assets/sw.js
@@ -1,47 +1,60 @@
-'use strict'
+"use strict";
 
-var CACHE_NAME = 'reedjones-cache-v1'
+var CACHE_NAME = "reedjones-cache-v1";
 var urlsToCache = [
-  '/',
-  '/css/style.min.css',
-  '/js/main.min.js',
-  '/images/bg/hero.jpg',
-  '/images/bg/impulse.jpg',
-  '/images/projects/controller.png',
-  '/images/projects/maze.png',
-  '/images/projects/minesweep.jpg',
-  '/images/projects/movies.jpg',
-  '/images/projects/vmas.jpg',
-  '/images/column/1.jpg',
-  '/images/column/2.jpg',
-  '/images/column/4.jpg',
-  '/images/column/6.png',
-  '/images/column/7.jpg',
-  '/images/cathedral.jpg',
-  '/fonts/icomoon.woff'
-]
+  "/",
+  "/css/style.min.css",
+  "/js/main.min.js",
+  "/images/bg/hero.jpg",
+  "/images/bg/impulse.jpg",
+  "/images/projects/controller.png",
+  "/images/projects/maze.png",
+  "/images/projects/minesweep.jpg",
+  "/images/projects/movies.jpg",
+  "/images/projects/vmas.jpg",
+  "/images/column/1.jpg",
+  "/images/column/2.jpg",
+  "/images/column/4.jpg",
+  "/images/column/6.png",
+  "/images/column/7.jpg",
+  "/images/cathedral.jpg",
+  "/fonts/icomoon.woff",
+];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache')
-        return cache.addAll(urlsToCache)
-      })
-  )
-})
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response
-        }
-        return fetch(event.request)
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
 
-        // return response ? response : fetch(event.request)
-      })
-  )
-})
+      return fetch(event.request);
+
+      // return response ? response : fetch(event.request)
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  var cacheKeeplist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,78 @@
 // 'use strict';
 
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  cleanCSS = require('gulp-clean-css'),
-  uglify = require('gulp-uglify'),
-  connect = require('gulp-connect'),
-  htmlmin = require('gulp-htmlmin'),
-  concat = require('gulp-concat'),
-  rename = require('gulp-rename');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  cleanCSS = require("gulp-clean-css"),
+  uglify = require("gulp-uglify"),
+  connect = require("gulp-connect"),
+  htmlmin = require("gulp-htmlmin"),
+  concat = require("gulp-concat"),
+  rename = require("gulp-rename");
 
 /************************* DEFAULT *************************************/
 
-
-
-
-gulp.task('connect', () => {
+gulp.task("connect", () => {
   connect.server({
-    root: '_public',
-    livereload: true
+    root: "_public",
+    livereload: true,
   });
 });
 
 // /**************************** SCSS *************************************/
-gulp.task('watch-sass', () => {
-  gulp.watch('scss/**/*.scss', ['sass']);
+gulp.task("watch-sass", () => {
+  gulp.watch("scss/**/*.scss", ["sass"]);
 });
-gulp.task('sass', () => {
-  return gulp.src('scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+gulp.task("sass", () => {
+  return gulp
+    .src("scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('_public/css'))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("_public/css"))
     .pipe(connect.reload());
 });
 
 // /************************** JAVASCRIPT ***************************************/
-gulp.task('watch-js', () => {
-  gulp.watch('scripts/**/*.js', ['concat-js']);
+gulp.task("watch-js", () => {
+  gulp.watch("scripts/**/*.js", ["concat-js"]);
 });
 
-gulp.task('concat-js', done => {
-  return gulp.src(['scripts/jquery.min.js', 'scripts/plugins.js', 'scripts/scripts.js'])
-    .pipe(concat('main.min.js'))
+gulp.task("concat-js", (done) => {
+  return gulp
+    .src(["scripts/jquery.min.js", "scripts/plugins.js", "scripts/scripts.js"])
+    .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest('_public/js'))
+    .pipe(gulp.dest("_public/js"))
     .pipe(connect.reload());
 });
 // /******************************* HTML **************************************/
-gulp.task('watch-html', () => {
-  gulp.watch('html/**/*.html', ['html-min']);
+gulp.task("watch-html", () => {
+  gulp.watch("html/**/*.html", ["html-min"]);
 });
 
-gulp.task('html-min', done => {
-  return gulp.src('html/**/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(gulp.dest('_public'))
+gulp.task("html-min", (done) => {
+  return gulp
+    .src("html/**/*.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest("_public"))
     .pipe(connect.reload());
 });
 
 /**************************** STATIC *************************************/
-gulp.task('copy', done => {
-  return gulp.src('assets/**/*')
-    .pipe(gulp.dest('_public/'));
+gulp.task("copy", (done) => {
+  return gulp.src("assets/**/*").pipe(gulp.dest("_public/"));
 });
 
-gulp.task('build', gulp.series('copy', 'sass', 'concat-js', 'html-min'));
-gulp.task('default', gulp.series('build', 'connect', 'watch-sass', 'watch-js', 'watch-html'));
+gulp.task("build", gulp.series("copy", "sass", "concat-js", "html-min"));
+gulp.task(
+  "default",
+  gulp.series("build", "connect", "watch-sass", "watch-js", "watch-html")
+);
